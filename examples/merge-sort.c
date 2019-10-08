f02c86cbf421211cb667a5765e4a855d88da3e83
yoyi <colinyoyo26>
2019-10-08 21:36:40 +0800

    Remove main function

--- examples/merge-sort.c
+++ examples/merge-sort.c
@@ -4,6 +4,8 @@
 #include "list.h"
 #include "common.h"
 
+
+
 static void list_merge(struct list_head *left,
                        struct list_head *right,
                        struct list_head *head)
@@ -21,7 +23,7 @@ static void list_merge(struct list_head *left,
             list_move_tail(&Rentry->list, head);
     }
 }
-static void list_msort(struct list_head *head)
+void list_msort(struct list_head *head)
 {
     struct list_head list_left, list_right, *tmp;
     struct listitem *item = NULL, *is = NULL;
@@ -43,39 +45,4 @@ static void list_msort(struct list_head *head)
     list_msort(&list_left);
     list_msort(&list_right);
     list_merge(&list_left, &list_right, head);
-}
-
-int main(int argc, char *argv[])
-{
-    struct list_head testlist;
-    struct listitem *item;
-    uint16_t values[atoi(argv[1])];
-
-    random_shuffle_array(values, (uint16_t) ARRAY_SIZE(values));
-    INIT_LIST_HEAD(&testlist);
-    assert(list_empty(&testlist));
-
-    for (size_t i = 0; i < ARRAY_SIZE(values); i++) {
-        item = (struct listitem *) malloc(sizeof(struct listitem));
-        assert(item);
-        item->i = values[i];
-        list_add_tail(&item->list, &testlist);
-    }
-
-    assert(!list_empty(&testlist));
-
-    qsort(values, ARRAY_SIZE(values), sizeof(values[0]), cmpint);
-    list_msort(&testlist);
-
-    /* check if the result is correct */
-    struct listitem *is = NULL, *item2 = NULL;
-    size_t j = 0;
-
-    list_for_each_entry_safe (item, is, &testlist, list) {
-        assert(item->i == values[j]);
-        list_del(&item->list);
-        free(item);
-        j++;
-    }
-    return 0;
 }
