99b43590f75b2bf5e501a4362598c423d5fc27d6
yoyi <colinyoyo26>
2019-10-11 13:57:08 +0800

    Separate three linked list & Rewrite the macro

--- examples/benchmark.c
+++ examples/benchmark.c
@@ -7,24 +7,10 @@
 #include "list.h"
 #include "benchmark.h"
 
-#define MERGE_ANALYSIS                               \
+#define ANALYSIS(name)                               \
     do {                                             \
         clock_gettime(CLOCK_REALTIME, &start);       \
-        list_msort(&testlist);                       \
-        clock_gettime(CLOCK_REALTIME, &end);         \
-        printf("%lu ", end.tv_nsec - start.tv_nsec); \
-    } while (0)
-#define QUICK_ANALYSIS                               \
-    do {                                             \
-        clock_gettime(CLOCK_REALTIME, &start);       \
-        list_qsort(&testlist);                       \
-        clock_gettime(CLOCK_REALTIME, &end);         \
-        printf("%lu ", end.tv_nsec - start.tv_nsec); \
-    } while (0)
-#define INSERT_ANALYSIS                              \
-    do {                                             \
-        clock_gettime(CLOCK_REALTIME, &start);       \
-        list_insertsort(&testlist);                  \
+        list_##name##sort(&name##_list);             \
         clock_gettime(CLOCK_REALTIME, &end);         \
         printf("%lu ", end.tv_nsec - start.tv_nsec); \
     } while (0)
@@ -32,8 +18,8 @@
 
 int main(int argc, char **argv)
 {
-    struct list_head testlist;
-    struct listitem *item;
+    struct list_head m_list, q_list, insert_list;
+    struct listitem *item1, *item2, *item3;
     struct timespec start, end;
     uint16_t values[atoi(argv[1])];
     uint16_t method = atoi(argv[2]);
@@ -42,35 +28,43 @@ int main(int argc, char **argv)
 #elif INCREASE
     increasing_array(values, (uint16_t) ARRAY_SIZE(values));
 #endif
-    INIT_LIST_HEAD(&testlist);
-    assert(list_empty(&testlist));
+    INIT_LIST_HEAD(&m_list);
+    INIT_LIST_HEAD(&q_list);
+    INIT_LIST_HEAD(&insert_list);
+    assert(list_empty(&m_list) && list_empty(&q_list) &&
+           list_empty(&insert_list));
 
     for (size_t i = 0; i < ARRAY_SIZE(values); i++) {
-        item = (struct listitem *) malloc(sizeof(struct listitem));
-        assert(item);
-        item->i = values[i];
-        list_add_tail(&item->list, &testlist);
+        item1 = (struct listitem *) malloc(sizeof(struct listitem));
+        item2 = (struct listitem *) malloc(sizeof(struct listitem));
+        item3 = (struct listitem *) malloc(sizeof(struct listitem));
+        assert(item1 && item2 && item3);
+        item1->i = item2->i = item3->i = values[i];
+        list_add_tail(&item1->list, &m_list);
+        list_add_tail(&item2->list, &q_list);
+        list_add_tail(&item3->list, &insert_list);
     }
-    assert(!list_empty(&testlist));
+    assert(!list_empty(&m_list) && !list_empty(&q_list) &&
+           !list_empty(&insert_list));
     if (!strcmp("mq", argv[2])) {
-        MERGE_ANALYSIS;
-        QUICK_ANALYSIS;
+        ANALYSIS(m);
+        ANALYSIS(q);
     }
 
     if (!strcmp("mt", argv[2])) {
-        MERGE_ANALYSIS;
-        INSERT_ANALYSIS;
+        ANALYSIS(m);
+        ANALYSIS(insert);
     }
 
     if (!strcmp("qt", argv[2])) {
-        QUICK_ANALYSIS;
-        INSERT_ANALYSIS;
+        ANALYSIS(q);
+        ANALYSIS(insert);
     }
 
     if (!strcmp("all", argv[2])) {
-        MERGE_ANALYSIS;
-        QUICK_ANALYSIS;
-        INSERT_ANALYSIS;
+        ANALYSIS(m);
+        ANALYSIS(q);
+        ANALYSIS(insert);
     }
     return 0;
 }
